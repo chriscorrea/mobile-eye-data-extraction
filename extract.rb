be55d8f6893ac844ba605f146de290f7aa162b7e
Christopher Correa <chriscorrea@gmail.com>
2009-02-25 13:51:50 -0500

    0.2

--- extract.rb
+++ extract.rb
@@ -1,22 +1,46 @@
 require 'rubygems'
+require 'ftools'
 #require 'rvideo'
 require 'csv'
 
 if ARGV.size == 1
   puts 'Data: ' + ARGV[0].to_s
   data_file = ARGV[0].to_s
-  video_file = nil
+  offset = nil
 elsif ARGV.size == 2
   puts 'Data: ' + ARGV[0].to_s
-  puts 'Video: ' + ARGV[0].to_s
+  puts 'Offset (in): ' + ARGV[1].to_s
     data_file = ARGV[0].to_s
-  video_file = ARGV[1].to_s
+  offset = ARGV[1].to_s
 else
-  raise "Syntax: extract.rb <datafile> <videofile>" if ARGV.size < 1  
+  raise "Expected: extract.rb <datafile> <optional offset>" if ARGV.size < 1  
+end
+
+file_break = (data_file =~ /\./)
+if data_file =~ /\./
+  puts "location: " + file_break.to_s
+  file_root = data_file[0,file_break]
+else
+  file_root = "_"
+end
+
+
+class Float
+  def round_to(x)
+    (self * 10**x).round.to_f / 10**x
+  end
+
+  def ceil_to(x)
+    (self * 10**x).ceil.to_f / 10**x
+  end
+
+  def floor_to(x)
+    (self * 10**x).floor.to_f / 10**x
+  end
 end
 
 class Fixation
-  attr_accessor :x, :y, :start, :duration, :id
+  attr_accessor :x, :y, :start, :centertime, :duration, :id, :offset
   
   #a new fixation requires x,y coordinates and a start time
   def initialize(x,y,start)  
@@ -24,28 +48,43 @@ class Fixation
        @y = y
        @start = start
        @duration = 1  
+       @offset = 0
   end
   
-  def formatted_timestamp
+  def formatted_old_timestamp
     @fields = @start.split(/\./) 
     return @fields[0] + '.' + (@fields[1].to_i * 10).to_s
   end
+  
+  
+  def formatted_timestamp
+    @fields = @centertime.split(/\./) 
+    #return @fields[0] + '.' + (@fields[1].to_i * 10).to_s
+    
+    #try returning 30 fps
+    @frame_adjusted = @fields[0] + '.' + (@fields[1].to_f.round_to(0) * 0.33).to_i.to_s
+    @fa = @frame_adjusted.split(/:/)
+    
+    return @fa[0].to_s + ':' + (@fa[1].to_i-(@offset.to_i)).to_s + ':' + @fa[2].to_s
+  end
+  
 end
 
 #SET GLOBAL PREF (these should be customizable eventually)
   #max allowable square of x coordinate difference between two frames of a single fixation
-  x_squared = 9
+  x_squared = 14
   #max allowable square of y coordinate difference between two frames of a single fixation
-  y_squared = 9
+  y_squared = 14
   #min consecutive frames to be saved as a valid fixation
   required_frames = 3
   #transform fraction of a second
-  sec_transform = 100
+  #sec_transform = 10
   
 puts 'Mobile Eye Data Extraction'
 puts 'By Christopher Correa'
-puts 'Version 0.1 (October 2008)'
+puts 'Version 0.2 (January 2009)'
 puts '-------'
+puts 'Analyzing ' + file_root + '...'
 
 #Intialize the results array (where we will store info about valid fixations)
 results = Array.new(0)
@@ -57,7 +96,8 @@ data.each do |d|
   
   #if there is not an old fixation and we start another potential fixation
   if @f.nil? 
-    @f = Fixation.new(-2000.0,-2000.0,0)    
+    @f = Fixation.new(-2000.0,-2000.0,0)
+        
   end
   
   if d[2].to_i > 0 && d[2].to_i < 1000 && d[3].to_i > -1000 && d[3].to_i < 1000 && (  ((d[2].to_f-@f.x)*(d[2].to_f-@f.x))>x_squared ||  ((d[3].to_f-@f.y)*(d[3].to_f-@f.y))>y_squared )
@@ -74,6 +114,12 @@ data.each do |d|
       
       #These is valid data here, so we are continuing an existing fixation and will increase duration
       @f.duration = @f.duration + 1
+      @f.x = d[2].to_f 
+      @f.y = d[3].to_f 
+      if(@f.duration == 2)
+        @f.offset = offset
+        @f.centertime =  d[0].to_s
+      end
   else
     #No good data here (missing or not valid)!
     
@@ -100,13 +146,26 @@ results.each do |r|
   puts '#' + r[0].to_s + ',' + r[1].to_s + ',' + r[2].to_s
 end
 
-if timecodes.size > 100
-  timecodes_file = File.new("newtimecodes.txt",'w+')
-  timecodes_file.puts timecodes
+puts 'Writing output files for ' + file_root + '...'
+
+# WRITE Timestamps
+  if offset.to_i>0
+    timecodes_file = File.new(file_root + "_timecodes_" + offset.to_s + ".txt",'w+')
+  else
+    timecodes_file = File.new(file_root + "_timecodes.txt",'w+')
+  end
+  
+timecodes_file.puts timecodes
+puts "- Created " + timecodes_file.inspect.to_s
+
+# WRITE data
+if offset.to_i>0
+  data_file = File.new(file_root + "_data_" + offset.to_s + ".txt",'w+')
+else
+  data_file = File.new(file_root + "_data.txt",'w+')
 end
 
+data_file.puts data
+puts "- Created " + data_file.inspect.to_s
 
-if data.size > 100
-  data_file = File.new("newdata.txt",'w+')
-  data_file.puts data
-end
+puts "Complete!"
